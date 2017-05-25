17b5fbc28b4bb1aed075c76625a0c29b1e604261
Michael Wender <michael@michaelwender.com>
2017-05-25 16:55:43 -0400

    Correctly calling `exit()`

--- lib/fns/rollback.php
+++ lib/fns/rollback.php
@@ -6,10 +6,10 @@ try {
     $result = $updater->rollback();
     if (! $result) {
         \cli\err( 'Unable to rollback. Rollback file not found!' );
-        exit 1;
+        exit(1);
     }
-    exit 0;
+    exit(0);
 } catch (\Exception $e) {
     \cli\err( 'Unable to rollback. Error: ' . $e );
-    exit 1;
+    exit(1);
 }
--- lib/fns/update.php
+++ lib/fns/update.php
@@ -8,13 +8,13 @@ try {
     $result = $updater->update();
     if (! $result) {
         \cli\line( 'No update needed.' );
-        exit 0;
+        exit(0);
     }
     $new = $updater->getNewVersion();
     $old = $updater->getOldVersion();
     \cli\line( sprintf( 'Updated from %s to %s', $old, $new ) );
-    exit 0;
+    exit(0);
 } catch (\Exception $e) {
     \cli\err( 'There was an error while trying to update.' );
-    exit 1;
+    exit(1);
 }
